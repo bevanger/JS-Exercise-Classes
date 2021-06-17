d5123fdb6167180df782601feec68ed911693cc3
bevanger <baileyevanger@gmail.com>
2021-06-17 14:56:59 -0600

    completed task 2, 3, and 4

--- index.js
+++ index.js
@@ -75,7 +75,25 @@ class Airplane {
   */
   
  class Car {
-    
+   constructor(model, milesPerGallon){
+     this.model = model;
+     this.milesPerGallon = milesPerGallon;
+     this.tank = 0;
+     this.odometer = 0;
+   }
+   fill(gallons){
+     this.tank += gallons;
+   }
+   drive(distance){
+     if(this.tank > (distance/this.milesPerGallon)){
+       this.odometer += distance;
+      this.tank -= (distance/this.milesPerGallon);
+     }else{
+       this.odometer += (this.tank * this.milesPerGallon);
+       this.tank = 0;
+       return `I ran out of fuel at ${this.odometer}!`;
+     }
+   }
   }
   
   /*
@@ -91,7 +109,14 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
-    
+    constructor(attributes) {
+      this.name = attributes.name;
+      this.age = attributes.age;
+      this.location = attributes.location;
+    }
+    speak(){
+      return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
   }
   
   /*
@@ -105,11 +130,22 @@ class Airplane {
       - The constructor calls the parent constructor passing it what it needs.
       - The constructor should also initialize `specialty`, `favLanguage` and `catchPhrase` properties on the instance.
       - Instructor instances have the following methods:
-          + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+          + `demo` receives a `subject` and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
-
+ class Instructor extends Lambdasian {
+   constructor(attributes){
+     super(attributes);
+     this.specialty = attributes.specialty;
+     this.favLanguage = attributes.favLanguage;
+     this.catchPhrase = attributes.catchPhrase;
+   }
+   demo(subject){
+    return `Today we are learning about ${subject}`
+   }
+   grade(student, subject){
+    return `${student.name} receives a perfect score on ${subject}`
+   }
  }
   /*
     TASK 5
